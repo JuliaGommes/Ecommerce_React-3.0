0ddfcb197956e95b92d803eb98812525be8f314b
Teemo2022 <julia_gommes@outlook.com>
2022-05-28 09:14:28 -0300

    alteracao pasta produtos

--- e-commerce-app/services/ServicoProduto.js
+++ e-commerce-app/services/ServicoProduto.js
@@ -3,23 +3,32 @@ const PRODUTOS = [
         id: 100,
         nome: 'Headset Redragon Zeus',
         preco: 850,
-        imagem: require('../assets/products/headset-100.jpg'),
+        imagem: require('../assets/produtos/headset-100.jpg'),
         descricao: 'Headset Gamer Feminino Redragon Zeus sem RGB'
     },
     {
         id: 101,
         nome: 'Teclado Mecanino Preto',
         preco: 600,
-        imagem: require('../assets/products/teclado-101.jpg'),
+        imagem: require('../assets/produtos/teclado-101.jpg'),
         descricao: 'Teclado Mecanico Preto com RGB'
     },
     {
         id: 102,
         nome: 'Mouse Gamer Redragon',
         preco: 350,
-        imagem: require('../assets/products/mouse-102.jpg'),
+        imagem: require('../assets/produtos/mouse-102.jpg'),
         descricao: 'Mouse Gamer Redragon com RGB'
-    }
+    },
+    {
+        id: 103,
+        nome: 'Cadeira Gamer Dx Racer',
+        preco: 3500,
+        imagem: require('../assets/produtos/cadeira-103.jpg'),
+        descricao: 'Cadeira Gamer Profissional DXRacer'
+    },
+
+
 ];
 export function getProdutos() {
     return PRODUTOS;
